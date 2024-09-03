0402e015885f7dba01ab50d1a1f5df6ce6e2e174
aditiaPutra1212 <125717426+aditiaPutra1212@users.noreply.github.com>
2024-09-03 11:52:09 +0700

    Add files via upload

---
+++ barcode-barang/kardus.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <title>Barang</title>
+</head>
+
+<body>
+  <section>
+    <img src="img\kardus.jpg" alt="">
+    <div class="tulisan">
+      <h1>Kardus</h1>
+      <h2>Jenis : Kertas</h2>
+      <h2>bentuk : Kotak</h2>
+    </div>
+  </section>
+
+  <button type="button" class="btn btn-secondary">Back</button>
+  <button id="next" type="button" class="btn btn-primary">Next</button>
